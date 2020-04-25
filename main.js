7465079411b3329e398961c09aac32a3caa9ff91
Leonid Arzumanyan <62353171+Leonid-A@users.noreply.github.com>
2020-04-25 20:40:28 +0400

    Local Storage (#1)
    * Local Storage
    
    * change main
    
    * check Harut

---
+++ main.js
@@ -0,0 +1,259 @@
+"use strict";
+
+const cardsConteiner = document.getElementById("parent-div");
+const displayCount = document.getElementById("step");
+const displayTimer = document.getElementById("timer");
+const startGame = document.getElementById("st-button");
+const fullReset = document.getElementById("res-button");
+const alertDisp = document.getElementById("alert-place");
+let userName = document.getElementById("fname");
+const winnersList = document.getElementById("user-scores");
+const bestScoresDisplay = document.getElementById("best-scores");
+const screenChange = document.getElementsByClassName("screen-change");
+const winAlert = document.getElementsByClassName("win-alert");
+const playAgain = document.getElementById("play-again");
+const endGame = document.getElementById("end-game");
+const letters = /^[a-zA-Z]+$/;
+let players = [];
+let bestScores = [];
+let timerInterval;
+const appMainObj = {
+    cardsImages: [
+        '<i data-id="1" class="fas fa-poo"></i>', 
+        '<i data-id="1" class="fas fa-poo"></i>',
+        '<i data-id="2" class="fas fa-hand-holding-usd"></i>',
+        '<i data-id="2" class="fas fa-hand-holding-usd"></i>',
+        '<i data-id="3" class="fas fa-dove"></i>',
+        '<i data-id="3" class="fas fa-dove"></i>',
+        '<i data-id="4" class="fas fa-baby-carriage"></i>',
+        '<i data-id="4" class="fas fa-baby-carriage"></i>',
+        '<i data-id="5" class="fas fa-grin-tongue"></i>',
+        '<i data-id="5" class="fas fa-grin-tongue"></i>',
+        '<i data-id="6" class="fas fa-hand-peace"></i>',
+        '<i data-id="6" class="fas fa-hand-peace"></i>',
+        '<i data-id="7" class="fas fa-anchor"></i>',
+        '<i data-id="7" class="fas fa-anchor"></i>',
+        '<i data-id="8" class="fas fa-dragon"></i>',
+        '<i data-id="8" class="fas fa-dragon"></i>'
+    ],
+    frontImage : "<i class='fab fa-js-square'></i>",
+    uIcardItems : [],
+    firstCard : null,
+    secondCard : null,
+    clickCount : 0,
+    openedCouple : 0,
+    counterForScreen: 0,
+    timerMinutes : 0,
+    timerSeconds : 0,
+    gamerName : "",
+    allowPlay : false,
+    newPlayer: {},
+    cupArr:['<i class="fas fa-trophy gold"></i>','<i class="fas fa-trophy silver"></i>','<i class="fas fa-trophy bronze"></i>']
+}
+
+start()
+    // game started
+function start(){
+    shuffleandDrow();
+    setuICardItems();
+    listenOnClick();
+    drawBestScores();
+    fullReset.addEventListener("click", resetAll,false);
+    startGame.addEventListener("click", gameStarted,false);
+    playAgain.addEventListener("click", startAgain,false);
+    endGame.addEventListener("click", resetAll,false);
+}
+
+    // cards shuffle and taking all card by className
+function shuffleandDrow() {
+    let allCards = ""; 
+    const imgs = appMainObj.cardsImages;
+    for(let j, hertakanItem, i = imgs.length-1; i > -1; --i){
+        const numRndm = parseInt(Math.random() * i);
+        hertakanItem = imgs[i];
+        imgs[i] = imgs[numRndm];
+        imgs[numRndm] = hertakanItem;
+        allCards += `<div class='card-item'>
+        <span class='front-side'>${appMainObj.frontImage}</span><span class='back-side'>${imgs[i]}</span></div>` ;
+    };
+    cardsConteiner.innerHTML = allCards;
+};
+
+function setuICardItems(){
+    appMainObj.uIcardItems = document.getElementsByClassName("card-item");
+};
+
+    //adding click event to cards
+function listenOnClick(){
+    for (let i=0, length = appMainObj.uIcardItems.length; i<length; i++) {
+        let everyItem = appMainObj.uIcardItems[i];
+        everyItem.addEventListener("click", flipCard, false);
+    }
+}
+
+    //flip card main function
+function flipCard(event){
+    if (appMainObj.firstCard === event.currentTarget) {
+       return;
+    }
+    if (appMainObj.clickCount === 0){
+        if (!appMainObj.allowPlay){
+            userName.style.borderColor = "red";
+            alertDisp.innerText = "Please type your name";
+            return;
+        } 
+        addCountAndFlip()
+        appMainObj.firstCard = event.currentTarget;
+    }
+    else if (appMainObj.clickCount === 1)  {
+        addCountAndFlip()
+        appMainObj.secondCard = event.currentTarget;
+        appMainObj.counterForScreen++;
+        displayCount.innerText = "Step: " + appMainObj.counterForScreen;
+        checkWin()
+    }
+}
+
+function checkWin(){
+    const fId = appMainObj.firstCard.querySelector('.back-side > i').getAttribute("data-id");
+    const sId = appMainObj.secondCard.querySelector('.back-side > i').getAttribute("data-id");
+    if (fId === sId){
+        appMainObj.firstCard.removeEventListener("click", flipCard, false);
+        appMainObj.secondCard.removeEventListener("click", flipCard, false);
+        appMainObj.openedCouple++;
+        clearCouple();
+        if (appMainObj.openedCouple === 8){
+            startGame.addEventListener("click", gameStarted,false);
+            startGame.removeAttribute("disabled");
+            userName.removeAttribute("disabled");
+            addPlayersScores();
+            addBestScores();
+            stopTimer();
+            alertShow();
+        }     
+    }
+    else{
+        setTimeout(()=>{
+            appMainObj.firstCard.classList.remove("flip");
+            appMainObj.secondCard.classList.remove("flip");
+            clearCouple();
+        },800);
+    }
+}
+
+function alertShow(){
+    screenChange[0].classList.add("screen-change-active");
+    winAlert[0].classList.add("win-alert-active");
+}
+
+    //counting and give class flip
+function addCountAndFlip() {
+    appMainObj.clickCount++;
+    event.currentTarget.classList.add("flip");
+}
+
+    // clear card counter and cards
+function clearCouple(){
+    appMainObj.clickCount = 0;
+    appMainObj.firstCard = null;
+    appMainObj.secondCard = null;
+}
+    //stop timer
+function stopTimer() {
+    clearInterval(timerInterval);
+    appMainObj.timerMinutes = 0;
+    appMainObj.timerSeconds = 0;
+}
+
+    //listing cards for removing flips and givein click event
+function listingCards(){
+    for (let i=0, length = appMainObj.uIcardItems.length; i<length; i++) {
+        let everyItem = appMainObj.uIcardItems[i];
+        everyItem.classList.remove("flip");
+        everyItem.addEventListener("click", flipCard, false);
+    };
+};
+
+    //function for clear inpute div
+function firstPosition(){
+    userName.style.borderColor = "initial";
+    userName.value = "";
+}
+
+    //reset button
+function resetAll(){
+    screenChange[0].classList.remove("screen-change-active");
+    winAlert[0].classList.remove("win-alert-active");
+    stopTimer();
+    displayTimer.innerHTML = "Timer: 0m 0s"
+    shuffleandDrow();
+    firstPosition();
+    alertDisp.innerText = "";
+    clearCouple();
+    listingCards();
+    appMainObj.allowPlay = false;
+    startGame.addEventListener("click", gameStarted,false);
+    startGame.removeAttribute("disabled");
+    userName.removeAttribute("disabled");
+    appMainObj.gamerName = "";
+    clearSteps()
+    winnersList.innerHTML = "";
+}
+
+function checkUserName(){
+    if (!userName.value.match(letters)){
+        userName.style.borderColor = "red";
+        alertDisp.innerText = "Type only letters";
+        return false;
+    } 
+    else if (userName.value.length > 10) {
+        userName.style.borderColor = "red";
+        alertDisp.innerText = "Type maximum 10 letters";
+        return false;
+    }
+    return true;
+}
+
+function clearSteps(){
+    appMainObj.counterForScreen = 0;
+    displayCount.innerText = "Step: " + appMainObj.counterForScreen;
+    appMainObj.openedCouple = 0;
+}
+
+function startAgain(){
+    userName.value = appMainObj.gamerName;
+    screenChange[0].classList.remove("screen-change-active");
+    winAlert[0].classList.remove("win-alert-active");
+    gameStarted();
+    console.log(userName.value)
+}
+
+    //start button with timer
+function gameStarted(){
+    shuffleandDrow();
+    clearSteps()
+    if (checkUserName()){
+        alertDisp.innerText = "Hello " + userName.value;
+        appMainObj.allowPlay = true;
+        startGame.removeEventListener("click", gameStarted, false);
+        startGame.setAttribute("disabled",true)
+        userName.setAttribute("disabled",true)
+        listingCards();
+        appMainObj.gamerName = userName.value;
+        getPlayersScores();
+        firstPosition();
+        displayTimer.innerHTML = "Timer: 0m 0s";
+        timerInterval = setInterval(function (){
+            appMainObj.timerSeconds ++;
+            if (appMainObj.timerSeconds === 60){
+                appMainObj.timerMinutes++;
+                appMainObj.timerSeconds = 0;
+                if (appMainObj.timerMinutes === 60){
+                    setTimeout(() => alert("GAME OVER"));
+                    resetAll();
+                }
+            }
+            displayTimer.innerHTML = `Timer: ${appMainObj.timerMinutes}m ${appMainObj.timerSeconds}s`;
+        },1000);
+    }
+}
---
+++ storage.js
@@ -0,0 +1,114 @@
+function addPlayersScores(){
+    let index = -1;
+    if (players){
+        index = players.findIndex(({name}) => name === appMainObj.gamerName);
+    }
+    else{
+        players = [];
+    }
+    
+    if (index === -1) {
+        index = players.length;
+        players[index] = {
+            name:appMainObj.gamerName,
+            scores: []
+        }
+    }
+    sortByIncrement(index);
+    sliceToTen(index);
+    drawWinnersTable(players[index]);
+    localStorage.setItem("winners", JSON.stringify(players));
+}
+
+function sortByIncrement(index){
+    let scoreIndex;
+    appMainObj.timerSeconds+=appMainObj.timerMinutes*60  ;
+    const playerScore = {
+        seconds: appMainObj.timerSeconds,
+        steps: appMainObj.counterForScreen
+    };
+    if (players[index].scores.length > 1){
+            scoreIndex = players[index].scores.findIndex(item => {
+            if ((item.seconds*item.steps) > (appMainObj.timerSeconds * appMainObj.counterForScreen)){return true};
+        });
+        if (scoreIndex !== -1){
+        players[index].scores.splice(scoreIndex,0,playerScore);
+        return;
+        }
+    }
+    players[index].scores.push(playerScore);
+}
+
+function sliceToTen(index){
+    if (players[index].scores.length>10){
+        players[index].scores= players[index].scores.slice(0,10);
+    }
+}
+
+function getPlayersScores(){
+    players = JSON.parse(localStorage.getItem("winners"));
+    if (players === null){
+        return;
+    };
+    const gamingPlayer = players.find(person => person.name == appMainObj.gamerName);
+    drawWinnersTable(gamingPlayer)
+};
+
+function drawWinnersTable(gamingPlayer) {
+    let winnersOutput = '';
+    if (gamingPlayer){
+        gamingPlayer.scores.forEach(item => {
+            let minutes = parseInt(item.seconds/60);
+            let seconds = item.seconds%60;
+            winnersOutput +=`<p class='user-name'>${gamingPlayer.name}</p><p class='user-time'>${minutes}m ${seconds}s</p><p class='user-steps'>${item.steps}</p>`;
+        });
+    } 
+    winnersList.innerHTML = winnersOutput;
+}
+
+function addBestScores(){
+    const newBest = {
+        name: appMainObj.gamerName,
+        scores: {
+            seconds:appMainObj.timerSeconds,
+            steps: appMainObj.counterForScreen
+        } 
+    }
+    let bestScoreIndex = -1;
+    if(bestScores){
+        bestScoreIndex = bestScores.findIndex(element => {
+            if (element.scores.seconds*element.scores.steps>appMainObj.timerSeconds*appMainObj.counterForScreen){
+                return true;
+            }
+        });
+    }
+    else{
+        bestScores = [];
+    };
+
+    if (bestScoreIndex === -1){
+        bestScores.push(newBest);
+    }
+    else{
+        bestScores.splice(bestScoreIndex,0,newBest);
+    };
+
+    if (bestScores.length >3) {
+        bestScores = bestScores.slice(0,3);  
+    };
+    localStorage.setItem("bestScores", JSON.stringify(bestScores));
+    drawBestScores();
+}
+
+function drawBestScores() {
+    bestScores = JSON.parse(localStorage.getItem("bestScores"));
+    let bestScoresOutput = "";
+    if (bestScores){
+        bestScores.forEach((item,index) => {
+            let minutes = parseInt(item.scores.seconds/60);
+            let seconds = item.scores.seconds%60;
+            bestScoresOutput+= `<p class='user-img'>${index+1}. ${appMainObj.cupArr[index]}</p><p class='user-name'>${item.name}</p><p class='user-time'>${minutes}m ${seconds}s</p><p class='user-steps'>${item.scores.steps}</p>`;
+        })
+    }
+    bestScoresDisplay.innerHTML =  bestScoresOutput;
+}
